c4697fbe740c7d48ab721dac83812115e0a7ea2d
HarunMwau <dittofarm7@gmail.com>
2024-05-07 21:52:37 +0300

    working rapper api

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const PORT = 8000 // || process.env.PORT 
+
+const rappers = {
+    '21 savage' : {
+        'age' : 31,
+        'birthName' : 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation' : 'London, England'
+    },
+    'chance the rapper' : {
+        'age' : 31,
+        'birthName' : 'Chancelor Johnathan Bennett',
+        'birthLocation' : 'Chicago, Illinois'
+    },
+    'kendrick lamar' : {
+        'age' : 36,
+        'birthName' : 'Kendrick Lamar Duckworth',
+        'birthLocation' : 'Compton, California'
+    },
+    'drake' : {
+        'age' : 37,
+        'birthName' : 'Aubrey Drake Graham',
+        'birthLocation' : 'Toronto, Ontario'
+    },
+    'unknown' : {
+        'age' : 0,
+        'birthName' : 'unknown',
+        'birthLocation' : 'unknown'
+    }  
+}
+
+app.get('/', (req,res)=> {
+    res.sendFile(__dirname + '/index.html') 
+})
+
+app.get('/api/:name', (req,res)=> {
+    const rapperName = req.params.name.toLowerCase()
+    if (rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['unknown'])
+    }
+})
+
+app.listen(PORT, ()=>{
+    console.log(`The server is now running on ${PORT}! Betta go catch it`)
+})
+
