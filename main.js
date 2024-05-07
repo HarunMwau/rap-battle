6f426e7395802c7065ca38b69dbc3547d81b3d74
HarunMwau <dittofarm7@gmail.com>
2024-05-08 00:47:34 +0300

    fix CORS

---
+++ main.js
@@ -0,0 +1,15 @@
+// alert('Working')
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://rap-battle.onrender.com/api/${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h2').innerText = data.birthname
+    }catch(error){
+        console.log(error)
+    }
+}
